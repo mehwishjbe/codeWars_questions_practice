b9782352f456f1d61e6e7969ebd9bd4a9b973cc0
mehwishjbe <mehwish.jbe@gmail.com>
2023-09-07 15:01:40 +0200

    question 6 solved

--- index.js
+++ index.js
@@ -63,3 +63,17 @@ console.log(result); */
 } 
 let output = mystery();
 console.log(output);  */
+
+
+/* Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake. */
+/* function greet(name){
+  if(name === "Johnny"){
+    return "Hello, " + name + " my love!";
+  }else{
+    return "Hello, " + name;
+  }
+}
+let message = greet("Johnny");
+let message1 = greet("Jimmy");
+console.log(message);
+console.log(message1); */
